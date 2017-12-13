2464fabc3d11522a677f09ce77af29b998cc01d7
smokeme <q8fawazo@hotmail.com>
2017-12-13 08:15:47 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ apt install virtualenv -y
 pip install fabric 
 adduser --disabled-password --gecos "" django
 echo -e "password4455\npassword4455\n" | sudo passwd django
-wget https://raw.githubusercontent.com/mhadam/testing-goat/master/deploy_tools/fabfile.py 
+wget https://raw.githubusercontent.com/smokeme/states/master/fabfile.py
 fab deploy
